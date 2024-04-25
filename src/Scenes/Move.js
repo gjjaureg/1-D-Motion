737ca96004740e64ea1b1b08bda048cc03923801
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-04-24 19:33:34 -0700

    first commit

---
+++ src/Scenes/Move.js
@@ -0,0 +1,20 @@
+class Move extends Phaser.Scene {
+    constructor() {
+        super("sceneMove");
+
+        this.my = {sprite: {}};  // Create an object to hold sprite bindings
+
+        //Create constants for the monster location
+        this.bodyX = 300;
+        this.bodyY = 350;
+    }
+    preload() {
+
+    }
+    create() {
+
+    }
+    update() {
+
+    }
+}
