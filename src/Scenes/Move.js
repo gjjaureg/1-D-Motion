bf44d30a36d49b209d410048a696c67be7514404
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-04-24 21:25:52 -0700

    spawn

--- src/Scenes/Move.js
+++ src/Scenes/Move.js
@@ -9,12 +9,22 @@ class Move extends Phaser.Scene {
         this.bodyY = 350;
     }
     preload() {
-
+        this.load.setPath("./assets/");
+        this.load.image('dog', "dog.png")
     }
     create() {
+        let my = this.my;
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "dog");
 
+        this.DKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.AKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     }
     update() {
-
+        if (this.DKey.isDown){     
+            my.sprite.body.x += 3;
+        }
+        else if (this.AKey.isDown) {
+            my.sprite.body.x -= 3;
+        }
     }
 }
--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ let config = {
     width: 800,
     height: 600,
     scene: [Move]
-    fps: { forceSetTimeOut: true, target: 30 }
+     
 }
 
 const game = new Phaser.Game(config);
